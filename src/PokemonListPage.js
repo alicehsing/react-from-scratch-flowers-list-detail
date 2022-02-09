6789720ae56f9c302673f64c89a02e80d89eabee
Alice Hsing <alicehsing@gmail.com>
2022-02-09 12:15:32 -0800

    add a search/filter bar using useEffect hook

--- src/PokemonListPage.js
+++ src/PokemonListPage.js
@@ -9,7 +9,9 @@ export default function PokemonsPage() {
   const [pokemons, setPokemons] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const perPage = 25;
-
+  const [currentQuery, setCurrentQuery] = useState('');
+  const [filteredPokemon, setFilteredPokemon] = useState([]);
+  
   useEffect(() => {
     async function fetch() {
       const from = perPage * (currentPage - 1) + 1;
@@ -21,15 +23,30 @@ export default function PokemonsPage() {
     fetch();
   }, [currentPage]);
 
+  //STRETCH: FILTER/SEARCH BAR
+  useEffect(() => {
+    const tempFilter = pokemons.filter(pokemon => pokemon.pokemon.includes(currentQuery));
+    setFilteredPokemon(tempFilter);
+  }, [currentQuery, pokemons]);
+
   return (
     <div className='pokemons-list-page'>
+      <div className='search'>
+        <input value={currentQuery} placeholder='Search Pokemon' type='text' onChange={ e => setCurrentQuery(e.target.value)}/>
+      </div>
       <h3>Current Page: {currentPage}</h3>
       <button onClick={() => setCurrentPage(currentPage - 1)}
         disabled={currentPage === 1}>Previous Page</button>
       <button onClick={() => setCurrentPage(currentPage + 1)}
         disabled={pokemons.length < perPage}>Next Page</button>
       <div>
-        <PokemonsList pokemons={pokemons} />
+        <PokemonsList pokemons={
+
+          //STRETCH: FILTER/SEARCH BAR
+          filteredPokemon.length
+            ? filteredPokemon
+            : pokemons
+        } />
       </div>
     </div>
   );
